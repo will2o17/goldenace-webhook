8ae3fd08d762f47d3c1b35ee7fd4a078b7d5c9a7
will2o17 <w.simpson2012@gmail.com>
2025-11-25 22:25:57 +0700

    Create webhook.py

---
+++ webhook.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+import requests
+import os
+
+app = Flask(__name__)
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+@app.route("/", methods=["POST"])
+def webhook():
+    data = request.get_data(as_text=True)
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": data
+    }
+
+    requests.post(url, json=payload)
+
+    return "OK", 200
+
+@app.route("/", methods=["GET"])
+def home():
+    return "GoldenAce Webhook Running!", 200
